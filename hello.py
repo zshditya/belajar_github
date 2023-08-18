913f3415a38ab35e8521449294bd7ff563bc0c45
vilaluna <142505693+vilaluna@users.noreply.github.com>
2023-08-18 22:10:41 +0700

    Menambah perubahan berupa quote mlbb satu
    Menambah perubahan berupa quote dari karakter game yaitu marksman Layla, dan Popol dan Kupa

--- hello.py
+++ hello.py
@@ -14,3 +14,7 @@ print("aku adalah yin dan tidak akan pernah berubah apapun yang terjadi")
 print("contoh perubahan1")
 print("contoh perubahan2")
 print("contoh perubahan3")
+
+print("we can do it")
+
+print("popol dan kupa adalah sahabat terbaik di dunia")
