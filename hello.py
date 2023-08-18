705764ea7a7bff52575e7c17f7b184e1b547cc85
bowops <142175094+bowops@users.noreply.github.com>
2023-08-18 21:42:48 +0700

    Development (#7)
    * Menambah contoh perubahan 1
    
    Menambah contoh perubahan di dalam file
    
    * Menambah contoh perubahan 2
    
    Menambah contoh perubahan di dalam file
    
    * Menambah contoh perubahan 3
    
    Menambah contoh perubahan di dalam file

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
 print("hello world")
 
-items = ['mangga', 'kiwi', 'jeruk', 'nanas']
+items = ['one piece', 'naruto', 'sailor moon', 'doraemon']
 
 for item in items:
   print(item)
+
+loveCoding = True
+print(type(loveCoding))
+
+print("no need to be so jealous, i was born for this")
+print("aku adalah yin dan tidak akan pernah berubah apapun yang terjadi")
+
+print("contoh perubahan1")
+print("contoh perubahan2")
+print("contoh perubahan3")
