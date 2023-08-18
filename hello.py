54e10c645e31c76083b74557ffcf761f51754400
vilaluna <142505693+vilaluna@users.noreply.github.com>
2023-08-18 20:53:54 +0700

    Update quotes gussion dan yin
    Update quotes dari karakter game gussion dan yin kemudian tampilkan hasilnya ke konsol.

--- hello.py
+++ hello.py
@@ -7,3 +7,6 @@ for item in items:
 
 loveCoding = True
 print(type(loveCoding))
+
+print("no need to be so jealous, i was born for this")
+print("aku adalah yin dan tidak akan pernah berubah apapun yang terjadi")
